f930ebc309a6ac04f74024400a2829ffd0826541
esferrari <duferrari01@gmail.com>
2021-06-25 23:22:28 -0300

    Tests part 1

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,64 @@
+import { AppError } from "src/errors/AppError";
+import { CreateCategoryUseCase } from "src/modules/cars/UseCases/CreateCategory/CreateCategoryUseCase";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
+
+
+describe(" Authenticate User", () => {
+    let authenticateUserUseCase: AuthenticateUserUseCase;
+    let usersRepositoryInMemory: UsersRepositoryInMemory;
+    let createUsersUseCase: CreateUserUseCase;
+    beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+
+        createUsersUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+
+    })
+    it("Should be able to authenticate an user", async () => {
+        const user: ICreateUserDTO = {
+            driver_license: "1234",
+            email: "user@test.com",
+            password: "3333",
+            name: "User Test"
+        };
+
+        await createUsersUseCase.execute(user);
+
+        const result = await authenticateUserUseCase.execute({
+            email:user.email,
+            password:user.password
+        })
+
+        expect(result).toHaveProperty("token");
+    })  
+
+    it("Should not be able to authenticate an no existent user", async () => {
+        expect(async () => {
+            await authenticateUserUseCase.execute({
+                email:"false@email.com",
+                password: "111"
+            })
+        }).rejects.toBeInstanceOf(AppError);
+    })
+
+    it("Should not be able to authenticate with incorrect password", () => {
+        expect(async () => {
+            const user: ICreateUserDTO = {
+                driver_license:"9999",
+                email:"user@user.com",
+                password: "12345",
+                name:"User test Error"
+            }
+
+            await createUsersUseCase.execute(user);
+
+            await authenticateUserUseCase.execute({
+                email:user.email,
+                password: "54321"
+            })
+        }).toBeInstanceOf(AppError);
+    })
+})
--- src/modules/cars/UseCases/CreateCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/UseCases/CreateCategory/CreateCategoryUseCase.spec.ts
@@ -1,17 +1,54 @@
+import { AppError } from "src/errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
 
 
-describe("Criar uma categoria", () => {
-    it("Espero que 2 + 2 seja 4", () => {
-        const soma = 2+2;
-        const resultado = 4;
+describe("Create Category", () => {
 
-        expect(soma).toBe(resultado);
+    let createCategoryUseCase: CreateCategoryUseCase;
+    let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+        
     })
 
-    it("Espero que 2 + 2 não seja 5", () => {
-        const soma = 2+2;
-        const resultado = 5;
+    it("Should be able to create a new category", async () => {
+        const category = {
+            name: "Category Test",
+            description: "Category Description Test",
+        }
+        await createCategoryUseCase.execute({
+            name: category.name,
+            description: category.description
+        })
 
-        expect(soma).not.toBe(resultado);
+        const categoryCreated = await categoriesRepositoryInMemory.findByname(category.name)
+
+        
+        expect(categoryCreated).toHaveProperty("id");
+
     })
+
+    it("Should not be able to create a new category with same name exists", async () => {
+        expect(async () => {
+            const category = {
+                name: "Category Test",
+                description: "Category Description Test",
+            }
+            await createCategoryUseCase.execute({
+                name: category.name,
+                description: category.description
+            })
+    
+            await createCategoryUseCase.execute({
+                name: category.name,
+                description: category.description
+            })
+        }).rejects.toBeInstanceOf(AppError);
+
+        
+
+    })
+
 })
